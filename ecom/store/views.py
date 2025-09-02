b348b85208328729cfc9214e2c1d5002b260705e
sjekob <144926291+sjekob@users.noreply.github.com>
2025-09-02 23:14:12 +0800

    Add files via upload

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from store.templates.store.forms import SignUpForm
+from django import forms
 
 
+def category_summary(request):
+    categories = Category.objects.all()
+    return render(request, 'category_summary.html', {'categories':categories})
 
+def category(request, foo):
+    foo = foo.replace('-', ' ')
+    try:
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products':products, 'category':category})
+    except:
+        messages.success(request, ("That Category Does Not Exist"))
+        return redirect('home')
+
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product':product})
 
 def home(request):
     products = Product.objects.all()
@@ -14,7 +34,7 @@ def about(request):
     return render(request, 'store/about.html', {})
 
 def login_user(request):
-    if request.method  =="POST":
+    if request.method  == "POST":
         username = request.POST['username']
         password = request.POST['username']
         user = authenticate(request, username=username ,password=password)
@@ -34,5 +54,21 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You have registered successfully!"))
+            return redirect('home')
+        else:
+            messages.success(request, ("There is a problem in your register, Please Try Again!"))
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form':form})
 
